86b8d392784b809afc3803bb31f2d69403c6b5ed
HarshiniMallepalle <harshinim0802@gmail.com>
2023-04-04 17:07:10 -0400

    First commit

---
+++ src/Object/MethodCallingFromDiffProject.java
@@ -0,0 +1,13 @@
+package Object;
+
+
+public class MethodCallingFromDiffProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	//First s =new First();
+
+	}
+
+}
---
+++ src/Object/Sum.java
@@ -0,0 +1,21 @@
+package Object;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		Sum Tsum = new Sum();
+		Tsum.addition(99, 40);
+
+
+	}
+
+	public void addition(int a, int b) {
+		String c;
+		c = a + b + "  Addition";
+		System.out.println("Addition of two nos. is :" + c);
+	}
+}
+
+
+
+
